c76c13b7e0a13590bbaedf00811d5b466598d888
Shubham-Solanki-agro <cropscienceshreeram@gmail.com>
2025-02-06 13:39:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const navLinks = document.querySelector(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    //Toggle Mobile Menu Visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
+
+//Close menu when the nav link is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => navLinks.click );
+});
+
+//Policy Popup
+function PolicyFunction() {
+    var policy = document.getElementById("myPopup");
+    policy.classList.toggle("show");
+  }
