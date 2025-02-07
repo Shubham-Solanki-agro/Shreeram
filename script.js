da51a9051607d3956310011d8f3bd746fc37f080
Shubham-Solanki-agro <cropscienceshreeram@gmail.com>
2025-02-07 13:05:45 +0530

    index

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navLinks = document.querySelector(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+img = document.getElementById("img1");
 
 menuOpenButton.addEventListener("click", () => {
     //Toggle Mobile Menu Visibility
@@ -19,4 +20,16 @@ navLinks.forEach(link => {
 function PolicyFunction() {
     var policy = document.getElementById("myPopup");
     policy.classList.toggle("show");
-  }
+}
+
+//Function to enlarge the image
+function enlargeImg() {
+            img.style.transform = "scale(1.5)";
+            img.style.transition = "transform 0.25s ease";
+}
+
+// Function to reset image size
+function resetImg() {
+            img.style.transform = "scale(1)";
+            img.style.transition = "transform 0.25s ease";
+}
